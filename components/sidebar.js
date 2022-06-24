7104de0dbf4bfcf11122cc46b34aabf2c121de71
Madan Bajgai <madanbajgai@gmail.com>
2022-06-24 21:20:06 +0545

    sidebar created

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,4 +1,5 @@
-import { Avatar } from "@mui/material";
+import { Chat, MoreVert, Search } from "@mui/icons-material";
+import { Avatar, Button, IconButton } from "@mui/material";
 import styled from "styled-components";
 
 export default function Sidebar() {
@@ -6,13 +7,59 @@ export default function Sidebar() {
     <Container>
       <Header>
         <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <MoreVert />
+        </IconsContainer>
       </Header>
+      <Searchdiv>
+        <Search />
+        <SearchInput placeholder="Search in chats" />
+      </Searchdiv>
+      <SidebarButton>Start a new chat</SidebarButton>
+
+      {/* List of chats */}
     </Container>
   );
 }
 
 const Container = styled.div``;
-const Header = styled.div``;
-const UserAvatar = styled(Avatar)`
-  margin: 10px;76yyt
+const SidebarButton = styled(Button)`
+  width: 100%;
+  border-top: 1px solid whitesmoke;
+  border-bottom: 1px solid whitesmoke;
 `;
+const SearchInput = styled.input`
+  outline: none;
+  border: none;
+  flex: 1;
+`;
+const Searchdiv = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconsContainer = styled.div``;
